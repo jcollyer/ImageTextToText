88fc81b054efb72ee4c527371c80fefca56d417a
jeremy collyer <collyerdesign@gmail.com>
2024-05-07 14:43:13 -0700

    move off vercel

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 import uvicorn
 from fastapi import FastAPI
+import io
+import requests
+from PIL import Image
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello from FastAPI!"}
+    r = requests.get('https://cdn.britannica.com/61/93061-050-99147DCE/Statue-of-Liberty-Island-New-York-Bay.jpg', stream=True)
+    aux_im = Image.open(io.BytesIO(r.content))
+
+
+    model_id = "vikhyatk/moondream2"
+    revision = "2024-04-02"
+    model = AutoModelForCausalLM.from_pretrained(
+        model_id, trust_remote_code=True, revision=revision
+    )
+    tokenizer = AutoTokenizer.from_pretrained(model_id, revision=revision)
+
+    # image = Image.open('<IMAGE_PATH>')
+    enc_image = model.encode_image(aux_im)
+    print(model.answer_question(enc_image, "Describe this image.", tokenizer))
+
+    # return {"message": "Hello from FastAPI!"}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
